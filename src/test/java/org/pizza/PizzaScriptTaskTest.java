9b7b00f94a4f35fa6959aeb5cca84a266c076346
Brendan Jackson <b.marc.j@gmail.com>
2017-03-26 10:25:27 +0100

    Example for task displays in test.

--- src/test/java/org/pizza/PizzaScriptTaskTest.java
+++ src/test/java/org/pizza/PizzaScriptTaskTest.java
@@ -1,10 +1,14 @@
 package org.pizza;
 
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pizza.service.MyApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -12,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
@@ -28,15 +33,24 @@ import static org.junit.Assert.assertNotNull;
 @IntegrationTest
 public class PizzaScriptTaskTest {
 
+    Logger LOGGER = LoggerFactory.getLogger(PizzaScriptTaskTest.class);
+
     @Autowired
     private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
 
     @Test
     public void submitPizzaOrderNotValid() {
         Map<String, Object> variableMap = new HashMap<>();
-        variableMap.put("amount", 11);
-        ProcessInstance processInstance  = runtimeService.startProcessInstanceByKey("simplePizza", variableMap);
+        variableMap.put("amount", 9);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("simplePizza", variableMap);
         assertNotNull(processInstance.getId());
+        List<Task> tasks = taskService.createTaskQuery().executionId(processInstance.getId()).list();
+        for (Task task : tasks) {
+            LOGGER.info("Task {}", task);
+        }
     }
 }
 
